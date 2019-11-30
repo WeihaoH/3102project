d59f45efb6d45e57d51f3cfa6f0818c846df4cb1
Haoyang_Liu96 <haoyangliu96@gmail.com>
2019-11-30 16:56:03 -0500

    Template the UI

--- TMS/TMS-ejb/target/generated-sources/annotations/TMS/ejb/persistence/Team_.java
+++ TMS/TMS-ejb/target/generated-sources/annotations/TMS/ejb/persistence/Team_.java
@@ -6,7 +6,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-11-30T00:22:49")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-11-30T16:50:19")
 @StaticMetamodel(Team.class)
 public class Team_ { 
 
